9500f64101e47707525d720146803ee91b1ce87b
TuTejsy <1111kaktus1111@gmail.com>
2019-07-31 00:14:02 +0300

    fix: language transfer

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import './styles/Search.css';
+import AuthorPreview from '../components/AuthorPreview'
 
 export default class Search extends Component {
   constructor() {
@@ -17,6 +18,9 @@ export default class Search extends Component {
         <div className="search-container">
           <input type="text" className="search-input" />
         </div>
+        <AuthorPreview lang={this.props.lang} />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
       </>
     )
   }
