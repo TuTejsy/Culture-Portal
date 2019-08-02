641b148b6b8cf79ea66985a482936ce2a9a7d3b0
e_yatsevich <katy.yats23@gmail.com>
2019-08-02 10:12:06 +0200

    feat: add basic AuthorPreview layout

--- src/components/AuthorPreview.js
+++ src/components/AuthorPreview.js
@@ -1,11 +1,33 @@
-import React from "react"
+import React, { Component } from "react";
 
+//import './styles/bootstrap.css';
 import './styles/AuthorPreview.css';
+//import data from '../data/data';
 
-const AuthorPreview = ({ lang }) => (
+const AuthorPreview = ({ lang }) => {
+  const language = { lang };
+
+  return (
+  
   <div className="author-container">
-    {lang}
+    <AuthorsCads />
   </div>
-);
+  )
+};
+
+const AuthorsCads = props => {
+  return (
+    <div className='card' style={{width: '18rem',}}>
+      <img src='' class='card-img-top' alt='' />
+      <div class='card-body'>
+        <h5 class='casd-title'></h5>
+        <p className='card-text'></p>
+        <p className='card-text'></p>
+        <p className='card-text'></p>
+        <a href='' class='btn btn-primary'></a>
+      </div>
+    </div>
+  )
+};
 
 export default AuthorPreview;
