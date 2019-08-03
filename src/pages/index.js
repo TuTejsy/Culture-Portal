58725af97229998e93cd7e557c58992356c0318d
Lidiya Gorohova <helga2011@yandex.ru>
2019-08-03 11:02:40 +0300

    docs: add main pade files

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,38 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import Image from "../components/Image"
 import SEO from "../components/Seo"
+import "../pages/styles.css"
 
 const IndexPage = ({ lang }) => (
   <Layout>
-    <SEO title="Main" />
-    <h1>{lang}</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="main-container">
+      <div class="buttons">
+          <a class="to-authors-list" href="#"> К списку авторов </a>
+          
+        </div>
+        <div class="main-page-text">
+          <p>Самобытная художественная культура Беларуси формировалась на протяжении столетий. 
+            Здесь существовали оригинальные архитектурные и художественные школы, создавались 
+            неповторимые музыкальные и литературные произведения.</p>
+          <p>Все дошедшие до наших дней шедевры белорусского искусства находятся под защитой государства. 
+            Они хранятся в коллекциях крупнейших белорусских музеев, собраниях библиотек. Классика 
+            белорусской музыки и драматургии демонстрируется на театральных подмостках и в концертных 
+            залах.</p>
+            <p>Список авторов, которые в данную минуту находятся на нашем сервере (5 человек). 
+              Здесь учитываются писатели, которые проживали на территории Беларусь и занимались 
+              своим творчеством на протяжении долгих лет.</p>
+        </div>
+        <div class="author-of-the-day">
+          <div class="title">Автор дня</div>
+          <div class="author-frame">
+            <div class="author-photo"> </div>
+            <div class="author-description">
+              <h3></h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
     </div>
-    <Link to="/search/">Go to page 2</Link>
   </Layout>
 )
 
