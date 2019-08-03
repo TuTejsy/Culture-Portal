5f43c3554f2751caaad64308848959cc9383e9e3
Qunapi <qunapi@mail.ru>
2019-08-03 22:31:40 +0300

    fix: map css position

--- src/components/MapWidget.js
+++ src/components/MapWidget.js
@@ -1,26 +1,26 @@
 import React from "react"
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react"
+import './styles/map.css'
 
-class MapWidget extends React.Component{
-/*  constructor(props) {
+class MapWidget extends React.Component {
+  /*  constructor(props) {
     super(props);
   }*/
   render() {
-    const mapStyles = {
-      width: '640px',
-      height: '480px',
-    };
     return (
-      <Map
-        google={this.props.google}
-        zoom={12}
-        style={mapStyles}
-        initialCenter={{ lat: 53.914643, lng: 27.584608}}
-      >
-        <Marker position={{ lat: 53.872585, lng: 27.530270}} />
-      </Map>
-    );
+      <div className="map-container">
+        <Map
+          google={this.props.google}
+          zoom={12}
+          initialCenter={{ lat: 53.914643, lng: 27.584608 }}
+        >
+          <Marker position={{ lat: 53.872585, lng: 27.53027 }} />
+        </Map>
+      </div>
+    )
   }
 }
 
-export default GoogleApiWrapper({apiKey: 'AIzaSyCG4tHez2c6c2WNOJXlagO5tNHM9ajX5r0' })(MapWidget);
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCG4tHez2c6c2WNOJXlagO5tNHM9ajX5r0",
+})(MapWidget)
