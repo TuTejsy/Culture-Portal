bec2fdb73f50678a30b7b34ebb19f62ac6b6aa4b
VladMeleshko <vladik4.98@mail.ru>
2019-08-03 10:34:57 +0300

    refactor: html for gallery and gallery.css

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -7,46 +7,46 @@ import './styles/gallery.css'
 function photos() {
     const gallery = <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
     <ol className="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+    <li id="1" className="active"></li>
+    <li id="2"></li>
+    <li id="3"></li>
+    <li id="4"></li>
+    <li id="5"></li>
+    <li id="6"></li>
+    <li id="7"></li>
+    <li id="8"></li>
+    <li id="9"></li>
+    <li id="10"></li>
     </ol>
     <div className="carousel-inner">
-    <div className="carousel-item active">
+    <div className="carousel-item active" id="photo1">
         <img src={data.authors[2].gallery[0]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo2">
         <img src={data.authors[2].gallery[1]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo3">
         <img src={data.authors[2].gallery[2]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo4">
         <img src={data.authors[2].gallery[3]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo5">
         <img src={data.authors[2].gallery[4]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo6">
         <img src={data.authors[2].gallery[5]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo7">
         <img src={data.authors[2].gallery[6]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo8">
         <img src={data.authors[2].gallery[7]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo9">
         <img src={data.authors[2].gallery[8]} className="d-block w-100" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" id="photo10">
         <img src={data.authors[2].gallery[9]} className="d-block w-100" alt="..." />
     </div>
     </div>
